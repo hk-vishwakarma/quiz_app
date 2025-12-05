a00425c4dff57e9b60c4b55d088cc584b2c0f901
Hemant Vishwakarma <hkvishwakarma01@gmail.com>
2025-12-05 14:05:56 +0530

    create models and add to admin

--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+# for questions 
+class Question(models.Model):
+    question_text = models.CharField(max_length=255)
+
+# for answer
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer_text = models.CharField(max_length=255)
+    is_true = models.BooleanField(default=False)
