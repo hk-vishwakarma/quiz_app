bed545091eb64ad3ec8398d4ad24a1c0b581dc9e
Hemant Vishwakarma <hkvishwakarma01@gmail.com>
2025-12-06 15:47:42 +0530

    authentication added

--- quiz/urls.py
+++ quiz/urls.py
@@ -3,4 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('', quiz_home, name = "home"),
+    path('register/', sign_up_view, name="register"),
+    path('sign-in/', sign_in_view, name="signin"),
+    path('logout/', logout_view, name = "logout"),
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 def quiz_home(request):
@@ -20,3 +23,64 @@ def quiz_home(request):
 
 
     return render(request, "quize.html", context={'questions' : questions})
+
+
+def sign_up_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        confirm_password = request.POST.get('confirm_password')
+
+
+        # to check that user already exists or not
+        user = User.objects.get(username = username)
+        if user.exists() :
+            messages.error(request, "username already exists !")
+            return redirect('register')
+        
+        # check both password is same
+        if password != confirm_password :
+            messages.error(request, "Password did not match !")
+            return redirect('register')
+        
+        User.objects.create_user(
+            username=username,
+            email=email,
+            password=password
+        )
+
+        messages.success(request=request, message="Register successfully !")
+        login(request, user)
+        return redirect('home')
+    
+    return render(request, "signup.html")
+
+
+# for login page
+def sign_in_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = User.objects.filter(username = username)
+        if not user.exists():
+            messages.error(request, "Username doest not exists !")
+            return redirect('signin')
+        
+        user = authenticate(username = username, password = password)
+
+        if user is None:
+            messages.error(request, "Password did not match")
+            return redirect('signin')
+        else:
+            login(request, user)
+            return redirect('home')
+        
+    return render(request, "signin.html")
+
+
+def logout_view(request):
+    logout(request=request)
+    return redirect('home')
+
